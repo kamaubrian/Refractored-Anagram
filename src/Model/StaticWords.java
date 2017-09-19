9755c0d7e75cc6e8149f99ebc9b57b737ad448fd
mtotodev05 <mtotodev05@gmail.com>
2017-09-19 17:33:58 +0300

    Implemented Netbeans_Words

---
+++ src/Model/StaticWords.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Brian-Kamau
+ */
+public final class StaticWords  extends wordLibrary{
+    
+    private static final String[] WORDS = {
+         "abstraction",
+        "ambiguous",
+        "arithmetic",
+        "backslash",
+        "bitmap",
+        "circumstance",
+        "combination",
+        "consequently",
+        "consortium",
+        "decrementing",
+        "dependency",
+        "disambiguate",
+        "dynamic",
+        "encapsulation",
+        "equivalent",
+        "expression",
+        "facilitate",
+        "fragment",
+        "hexadecimal",
+        "implementation",
+        "indistinguishable",
+        "inheritance",
+        "internet",
+        "java",
+        "localization",
+        "microprocessor",
+        "navigation",
+        "optimization",
+        "parameter",
+        "patrick",
+        "pickle",
+        "polymorphic",
+        "rigorously",
+        "simultaneously",
+        "specification",
+        "structure",
+        "lexical",
+        "likewise",
+        "management",
+        "manipulate",
+        "mathematics",
+        "hotjava",
+        "vertex",
+        "unsigned",
+        "traditional"      
+    };
+    
+    
+}
