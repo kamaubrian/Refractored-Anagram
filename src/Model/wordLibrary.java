9dc8ffc02a3d8334855d56482e1e6c89980526ef
mtotodev05 <mtotodev05@gmail.com>
2017-09-19 17:18:07 +0300

    Models

---
+++ src/Model/wordLibrary.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ * Refractor: -> Brian Kamau
+ */
+public abstract class wordLibrary {
+    
+    //Have Abstract Classes, that handle how words are handled in the program.
+    protected wordLibrary(){
+        
+        
+        
+    }
+    
+    
+}
